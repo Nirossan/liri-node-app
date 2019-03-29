73133ca7e6dd7ddf2caf8531f9895e623a4e68eb
Nirossan <46507846+Nirossan@users.noreply.github.com>
2019-03-28 20:59:50 -0400

    Add files via upload

---
+++ liri.js
@@ -0,0 +1,44 @@
+// Modules
+require("dotenv").config();
+var axios = require('axios')
+var moment = require('moment')
+var Spotify = require('node-spotify-api')
+
+// Environment Variables
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+console.log(process.env)
+
+//  switch (key) {
+//      case value:
+         
+//          break;
+ 
+//      default:
+//          break;
+//  }
+//  switch (key) {
+//      case value:
+         
+//          break;
+ 
+//      default:
+//          break;
+//  }
+//  switch (key) {
+//      case value:
+         
+//          break;
+ 
+//      default:
+//          break;
+//  }
+//  switch (key) {
+//      case value:
+         
+//          break;
+ 
+//      default:
+//          break;
+//  }
